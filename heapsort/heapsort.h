1542da116cfa82889e3bace27a31143754e9c3e3
Huijun Mai <m.maihuijun@gmail.com>
2012-09-13 22:25:46 +0800

    change name "heap" to "heapsort"

---
+++ heapsort/heapsort.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define PARENT(i) (i) / 2         //i's parent
+#define LEFT(i)   (i) * 2         //i's leftchild
+#define RIGHT(i)  (i) * 2 + 1     //i's rightchild
+#define ARRAY_LENGTH  5           //the length of array
+
+/* make the subtree rooted at index i become a max-heap */
+void max_heapify(int *array, int *i, int *heap_size);
+
+/* build the max-heap */
+void build_max_heap(int *array);
+
+/* the algorithm of heapsort */
+void heapsort(int *array);
